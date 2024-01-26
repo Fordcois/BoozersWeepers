558520eacdb37dbff391081c9c3903a4682dd436
Sam Ford <samueljamesford@googlemail.com>
2024-01-26 14:40:52 +0000

    Removes incorrect Log-in Console log

--- frontend/src/components/auth/LoginForm.js
+++ frontend/src/components/auth/LoginForm.js
@@ -16,10 +16,8 @@ const LogInForm = ({ navigate }) => {
     })
 
     if(response.status !== 201) {
-      console.log("Login succesful")
       navigate('/login')
     } else {
-      console.log("login unsuccessful")
       let data = await response.json()
       window.localStorage.setItem("token", data.token)
       navigate('/myAccount');
