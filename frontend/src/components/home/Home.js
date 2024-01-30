837a8c86c9ad37634a142136741b92642ae3922f
Sam Ford <samueljamesford@googlemail.com>
2024-01-30 11:30:33 +0000

    Adds Redirect from homepage if logged in

--- frontend/src/components/home/Home.js
+++ frontend/src/components/home/Home.js
@@ -1,21 +1,35 @@
 import VertNavbar from '../VertNavBar/VertNavBar';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import isTokenValid from '../Utility/isTokenValid';
+import BlackboardHeader from '../blackboardHeader/blackboardHeader';
 import '../../Pages/style.css'
-import '../header/Header.js'
+
 
 const Home = ({ navigate }) => {
   const [expanded, setExpanded] = useState(true);
+  const [token, setToken] = useState(window.localStorage.getItem("token"));
+  const [isLoggedIn, setIsLoggedIn] = useState(isTokenValid(token));
   const toggleExpand = () => {setExpanded(!expanded);};
 
+  useEffect(() => {
+    if (isLoggedIn) {navigate('/MyAccount');}
+  }, [isLoggedIn, navigate]);
+
   return(
-    <div className='logged-out-page-container'>
-    <VertNavbar expanded={expanded} toggleExpand={toggleExpand} />
-  <a className='register-link' href='/signup'>Please Register</a><br/>
-  or <br/>
-  <a className='login-link'href='/login'>Sign in</a>
-    
-  </div>
-  )
-}
+<div className='shade'>
+<div className={`page-content ${expanded ? 'shifted-content' : ''}`}>
+<div className='blackboard'>
+<div className='form'>
+  <VertNavbar expanded={expanded} toggleExpand={toggleExpand} />
+  <BlackboardHeader />
+          
+    <span className='chalktitle'>WELCOME TO BOOZERS WEEPERS</span>
+</div>
+</div>
+</div>
+</div>
+
+
+)}
 
 export default Home;
