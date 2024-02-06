6ae82764dc87f8c66678f48e7add7f5c6f8712fa
Sam Ford <samueljamesford@googlemail.com>
2024-02-06 10:07:04 +0000

    Cleans code for Wager Info Page

--- frontend/src/components/singleWagerPage/WagerInfoPage.js
+++ frontend/src/components/singleWagerPage/WagerInfoPage.js
@@ -6,9 +6,8 @@ import SinglePendingWager from './childComponents/SinglePendingWager';
 import SingleWagerRequest from './childComponents/SingleWagerRequest';
 import SingleOngoingWager from './childComponents/SingleOngoingWager';
 import SingleResolvedWager from './childComponents/SingleResolvedWager';
-import '../../Pages/style.css'
 import BlackboardHeader from '../blackboardHeader/blackboardHeader';
-
+import '../../Pages/style.css'
 
 
 const WagerInfoPage = ({ navigate }) => {
@@ -21,9 +20,8 @@ const WagerInfoPage = ({ navigate }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`/wagers/${wagerID}`, {
-          headers: { Authorization: `Bearer ${token}` }
-        });
+        const response = await fetch(`/wagers/${wagerID}`, 
+        {headers: { Authorization: `Bearer ${token}` }});
         
         if (!response.ok) { throw new Error('Network response was not ok');}
         
@@ -40,60 +38,39 @@ const WagerInfoPage = ({ navigate }) => {
 
 const toggleExpand = () => {setExpanded(!expanded);};
 
-
-if (!wagerData) {
-  return(
-    <div>
-    <VertNavbar expanded={expanded} toggleExpand={toggleExpand} />
-    <div className={`page-content ${expanded ? 'shifted-content' : ''}`}>
-      <p id='loading-message' className='loading-message'>Loading...</p>
-    </div>
-  </div>
-  )
-} else {
-
-  return (
-    <div className='shade'>
+return (
+  <div className='shade'>
     <div className={`page-content ${expanded ? 'shifted-content' : ''}`}>
       <div className='blackboard'>
         <div className='form'>
           <VertNavbar expanded={expanded} toggleExpand={toggleExpand} />
           <BlackboardHeader /> 
 
-
-
-
-
-          
-
-        
-    
-        <h1 id='single-wager-page-header' className='page_subheading'>Wager Details</h1>
-        { wagerData.approved === false && wagerData.peopleInvolved[0]._id === loggedInUser ? (
-      
-            <SinglePendingWager wagerData={wagerData}/>
-      
-            ) : wagerData.approved === false && wagerData.peopleInvolved[1]._id === loggedInUser ? (
-        
-            <SingleWagerRequest wagerData={wagerData}/>
-        
-            ) : wagerData.approved === true && wagerData.winner === null ? (
-          
-            <SingleOngoingWager wagerData={wagerData}/>
-          
-            ) : wagerData.winner !== null ? (
-            
-            <SingleResolvedWager wagerData={wagerData}/>
-            
-            ) : (
-            <p>Error - return to account page</p>
-        )}
+          {!wagerData ? (
+              <div style={{ 'textAlign': 'center' }}>
+                <span className="chalk" style={{ '--fsize': '34px' }}>No Wager Found</span>
+                <span className="chalk" style={{ '--fsize': '28px', 'opacity': '0.8' }}>(Sorry!)</span>
+              </div>
+          ) : (
+            <>
+              <h1 id='single-wager-page-header' className='page_subheading'>Wager Details</h1>
+              {wagerData.approved === false && wagerData.peopleInvolved[0]._id === loggedInUser ? (
+                <SinglePendingWager wagerData={wagerData}/>
+              ) : wagerData.approved === false && wagerData.peopleInvolved[1]._id === loggedInUser ? (
+                <SingleWagerRequest wagerData={wagerData}/>
+              ) : wagerData.approved === true && wagerData.winner === null ? (
+                <SingleOngoingWager wagerData={wagerData}/>
+              ) : wagerData.winner !== null ? (
+                <SingleResolvedWager wagerData={wagerData}/>
+              ) : (
+                <p>Error - return to account page</p>
+              )}
+            </>
+          )}
         </div>
       </div>
     </div>
   </div>
-      )
-    }
-  }
+);}
 
 export default WagerInfoPage;
