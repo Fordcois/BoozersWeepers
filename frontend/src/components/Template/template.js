4f93fd6c9938af228a85b90a4aee8dc82e73263f
Sam Ford <samueljamesford@googlemail.com>
2024-02-22 20:07:08 +0000

    Implements first steps of Query Params

--- frontend/src/components/Template/template.js
+++ frontend/src/components/Template/template.js
@@ -2,39 +2,18 @@ import React, { useEffect, useState } from 'react';
 import VertNavbar from '../VertNavBar/VertNavBar';
 import BlackboardHeader from '../blackboardHeader/blackboardHeader';
 import '../../Pages/style.css';
-import { useParams } from 'react-router-dom';
-
-import SingleUserStats from '../stats/getSingleUserStats';
+import { useLocation } from 'react-router-dom';
 
 const Template = ({ navigate }) => {
   const [token, setUserToken] = useState(window.localStorage.getItem('token'));
   const [expanded, setExpanded] = useState(true);
-  const [list,setlist] = useState([])
-  const [ID,setID] = useState('65b3cb2aa30533d477e17ff2')
-  const { name, lastName } = useParams();
   const toggleExpand = () => {
     setExpanded(!expanded);
   };
   
-  
-
-
-
-
-
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {const response = await fetch(`/wagers/findall/${ID}`, 
-  //     {headers: { Authorization: `Bearer ${token}` }});
-  //       if (!response.ok) {throw new Error('Network response was not ok');}
-  //       const userData = await response.json();
-  //       console.log(userData)
-  //   } catch (error) {console.error('Error fetching user data:', error);}
-  //   };
-
-  //   fetchData();
-  // }, [token]); 
+  const location = useLocation();
+  const nameParam = new URLSearchParams(location.search).get('name');
+  const name = nameParam || 'John Doe'; // Use 'John Doe' as default if nameParam is falsy
 
   return (
     <div className='shade'>
@@ -43,21 +22,10 @@ const Template = ({ navigate }) => {
           <div className='form'>
             <VertNavbar expanded={expanded} toggleExpand={toggleExpand} />
             <BlackboardHeader /> 
-            <span className='chalktitle'>Welcome to the Workshop, {name} {lastName} </span>
+          
+            <span className='chalktitle'>Welcome to the Workshop, {name} </span>
+            The status is {expanded.toString()}
 
-
-
-
-
-            
-
-          
-          
-          
-          
-          
-          
-          
           </div>
         </div>
       </div>
