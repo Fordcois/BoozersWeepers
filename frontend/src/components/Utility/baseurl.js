4a67d9a1a186d2b0eabadaaa76631df5557a99df
Sam Ford <samueljamesford@googlemail.com>
2024-05-23 18:50:47 +0100

    Changes Backend Address

--- frontend/src/components/Utility/baseurl.js
+++ frontend/src/components/Utility/baseurl.js
@@ -2,7 +2,7 @@ let baseUrl;
 
 if (process.env.NODE_ENV === 'production') {
   // Set the deployed URL
-    baseUrl = "https://boozersweepers.onrender.com";
+    baseUrl = "https://boozersweepers-api.onrender.com";
 } else {
   // Set the local URL
     baseUrl = "http://localhost:8080";
