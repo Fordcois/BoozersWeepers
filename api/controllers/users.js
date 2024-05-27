921fe8ea45398029d5d22abfb13c08a10a45b898
Sam Ford <samueljamesford@googlemail.com>
2024-05-27 19:16:50 +0100

    Encrypts Passwords using BCrypt

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
 const UsersController = {
   //SeverLoad IS ONLY RELEVANT FOR HOSTING ON RENDER.COM FREETIER
@@ -8,9 +9,17 @@ const UsersController = {
   },
 
   Create: (req, res) => {
-    const user = new User(req.body);
-    let passwordErrorMsgs = "";
 
+    const salt = bcrypt.genSaltSync(10);
+
+    const user = new User({
+      email: req.body.email,
+      username: req.body.username,
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      password: bcrypt.hashSync(req.body.password, salt)
+  });
+    let passwordErrorMsgs = "";
     if(user.password.length < 8) {
       passwordErrorMsgs += "Password must be at least 8 characters in length. \n"
     } 
@@ -24,20 +33,14 @@ const UsersController = {
 
     user.save((err) => {
       if (err) {
-        
         if(err.code === 11000){
           if(err.keyPattern.email){
             return res.status(400).json({message: 'Email is already in use'})
-
           }
           else if(err.keyPattern.username)
           return res.status(400).json({ message: 'Username is already in use' });
-
         }
-        else{
-          res.status(400).json({message: 'Bad request'})
-
-        }
+        else{res.status(400).json({message: 'Bad request'})}
       
       
       } else {
